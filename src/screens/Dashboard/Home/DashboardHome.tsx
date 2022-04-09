4242b62bbefb4ecf89434baca044153d975b3006
Esteban Trillo <esteban@devtrillo.com>
2022-04-09 14:50:16 -0500

    feat: animate list of bots

--- src/screens/Dashboard/Home/DashboardHome.tsx
+++ src/screens/Dashboard/Home/DashboardHome.tsx
@@ -1,11 +1,14 @@
 import cx from "classnames";
 import { collection, FieldValue, query, where } from "firebase/firestore";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { Link } from "react-router-dom";
 
 import { Card } from "@/components/Card";
 import CreateBotModal from "@/components/CreateBotModal/CreateBotModal";
+import { useVariants } from "@/hooks/useVariants";
 import { auth, firestore } from "@/utils/firebase";
 
 import style from "./DashboardHome.module.css";
@@ -26,35 +29,82 @@ function DashboardHome() {
     collection(firestore, "bots"),
     where("user", "==", user?.uid ?? "")
   );
-  const [botSnapshot] = useCollection(q);
+  const [botSnapshot, loading] = useCollection(q);
+
+  const cardVariants = useVariants({
+    exit: { opacity: 0, transition: { duration: 0.2 } },
+    hidden: { opacity: 0 },
+    visible: (i: number) => ({
+      opacity: 1,
+      transition: { delay: i * 0.2 },
+    }),
+  });
+  const containerVariants = useVariants({
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  });
 
   return (
-    <div>
-      <h1>Dashboard Home</h1>
-      <section className={cx("mt-10 gap-3", style.container)}>
-        {botSnapshot?.docs.map((doc) => {
-          const { name, description } = doc.data();
-          const id = doc.id;
-          return (
-            <Link key={name} to={`/dashboard/${id}`}>
+    <>
+      <div className="relative">
+        <h1>Dashboard Home</h1>
+
+        <AnimatePresence>
+          {loading ? (
+            <motion.div
+              key={0}
+              animate="visible"
+              className={cx("mt-10 gap-3 absolute", style.container)}
+              custom={0}
+              exit="exit"
+              initial="hidden"
+              variants={cardVariants}
+            >
               <Card
-                animate
                 horizontal
-                imgSrc={`https://avatars.dicebear.com/api/bottts/${name}.svg`}
+                imgSrc="https://avatars.dicebear.com/api/bottts/loading.svg"
               >
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {name}
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  {description}
-                </p>
+                <div>
+                  <div className="w-36 bg-slate-700 h-6 rounded-md opacity-25 animate-pulse" />
+                  <div className="w-full bg-slate-700 h-6 rounded-md opacity-25 mt-5 animate-pulse" />
+                </div>
               </Card>
-            </Link>
-          );
-        })}
-      </section>
+            </motion.div>
+          ) : (
+            <motion.section
+              animate="visible"
+              className={cx("mt-10 gap-3", style.container)}
+              exit="hidden"
+              initial="hidden"
+              variants={containerVariants}
+            >
+              {botSnapshot?.docs.map((doc, index) => {
+                const { name, description } = doc.data();
+                const id = doc.id;
+                return (
+                  <motion.div key={name} custom={index} variants={cardVariants}>
+                    <Link to={`/dashboard/${id}`}>
+                      <Card
+                        horizontal
+                        imgSrc={`https://avatars.dicebear.com/api/bottts/${name}.svg`}
+                      >
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          {name}
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                          {description}
+                        </p>
+                      </Card>
+                    </Link>
+                  </motion.div>
+                );
+              })}
+            </motion.section>
+          )}
+        </AnimatePresence>
+      </div>
       <CreateBotModal />
-    </div>
+    </>
   );
 }
 
