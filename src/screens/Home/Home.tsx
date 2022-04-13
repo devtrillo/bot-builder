a86f8410ad28f21f507f8aa2a1cc8c6baaa06d81
Esteban Trillo <esteban@devtrillo.com>
2022-04-13 11:03:51 -0500

    feat: update home page

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="dark:bg-gray-700 bg-gray-300 text-center">
-        Bienvenido a tu calculadora
-      </h1>
+      <h1 className="dark:bg-gray-700 bg-gray-300 text-center">DEMO</h1>
     </div>
   );
 };
