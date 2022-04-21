7f2a62bc10e94fc7be32b1cd582cae6b49408432
Esteban Trillo <esteban@devtrillo.com>
2022-04-21 10:45:56 -0500

    feat: fix tsc

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import reportWebVitals from "@/reportWebVitals";
 import App from "./App";
 
 const container = document.getElementById("root");
+if (!container) throw new Error("No root element found");
 const root = createRoot(container);
 
 root.render(
