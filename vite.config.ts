24f6796b5c9112386026ead2e31f9e4e59b7ffd7
Esteban Trillo <esteban@devtrillo.com>
2022-04-23 00:23:18 -0500

    feat: install some tests and the configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "import.meta.vitest": "undefined",
+  },
   envDir: "./config",
   plugins: [react()],
   resolve: {
     alias: { "@": resolve(__dirname, "src") },
+  },
+  test: {
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+    environment: "jsdom",
+    globals: true,
+    includeSource: ["src/**/*.{js,ts,jsx,tsx}"],
+    setupFiles: "./src/test/setup.ts",
   },
 });
