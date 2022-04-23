10b484d219bf6030a8cba1a0dfcab570eefe12f9
Esteban Trillo <esteban@devtrillo.com>
2022-04-23 01:19:40 -0500

    refactor: removed wierd space

--- src/utils/getMaxValue.ts
+++ src/utils/getMaxValue.ts
@@ -9,9 +9,7 @@ if (import.meta.vitest) {
 
   test.each([
     [[-1, -2, -3, -4, -5], -1],
-
     [[1, 2, 3, 4, 5], 5],
-
     [[1, 2, 3, 4, -5, 100, 300], 300],
   ])("The array %s max should be %d", (array, result) => {
     expect(getMaxValue(array)).toBe(result);
--- src/utils/getMinValue.ts
+++ src/utils/getMinValue.ts
@@ -8,9 +8,7 @@ if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
   test.each([
     [[-1, -2, -3, -4, -5], -5],
-
     [[1, 2, 3, 4, 5], 1],
-
     [[1, 2, 3, 4, -5, 100, 300], -5],
   ])("The array %s min should be %d", (array, result) => {
     expect(getMinValue(array)).toBe(result);
